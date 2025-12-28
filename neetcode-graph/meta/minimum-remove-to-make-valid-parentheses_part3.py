50f7d69c6cc260fadc503d55db9670e667caeedc
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2025-12-27 21:46:51 -0800

    what a crazy problem

---
+++ neetcode-graph/meta/minimum-remove-to-make-valid-parentheses_part3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        # time O(n) | space O(n)
+        stack = []
+        s = list(s)
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+
+        while stack:
+            s[stack.pop()] = ''
+
+        return ''.join(s)

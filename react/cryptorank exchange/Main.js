71fc46084a2bb2a4023a8db1b4f8ee9d6ff0da45
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2026-01-09 01:04:51 -0800

    more stuff

--- react/cryptorank exchange/Main.js
+++ react/cryptorank exchange/Main.js
@@ -2,22 +2,41 @@ import React from "react";
 import Table from "./Table";
 
 function Main() {
+    const [exchangeAmount, setExchangeAmount] = React.useState("");
+    const [touched, setTouched] = React.useState(false);
+
+    // if exchangeAmount is empty, less than 0.01 or exceeds 17042.67, show the appropriate error message
+    let error = "";
+    if (exchangeAmount === "" ) {
+        error = "Amount cannot be empty";
+    } else if (Number(exchangeAmount) < 0.01) {
+        error = "Amount cannot be less than $0.01";
+    } else if (Number(exchangeAmount) > 17042.67) {
+        error = "Amount cannot exceed the available balance";
+    }
+
+    const handleChange = (e) => {
+        setTouched(true);
+        setExchangeAmount(e.target.value);
+    }
+
     return (
         <div className="layout-column align-items-center mx-auto">
         <h1>CryptoRank Exchange</h1>
         <section>
             <div className="card-text layout-column align-items-center mt-12 px-8 flex text-center">
             <label>
-                I want to exchange $ <input className="w-10" data-testid="amount-input" required type="number" placeholder="USD" value={0} /> of my $
+                I want to exchange $ <input className="w-10" data-testid="amount-input" required type="number" placeholder="USD" value={exchangeAmount} 
+                onChange={handleChange}/> of my $
                 <span>17042.67</span>:
             </label>
-            <p data-testid="error" className="form-hint error-text mt-3 pl-0 ml-0">
-                {"Amount cannot be empty"}
+            <p data-testid="error" className="form-hint error-text mt-3 pl-0 ml-0" style={{visibility: touched && error ? 'visible' : 'hidden'}}>
+                {error}
             </p>
             {/* The errors can be Amount cannot be empty /be less than $0.01/exceed the available balance */}
             </div>
         </section>
-        <Table />
+        <Table exchangeAmount={exchangeAmount}/>
         </div>
     );
 }
--- react/cryptorank exchange/Table.js
+++ react/cryptorank exchange/Table.js
@@ -1,7 +1,40 @@
 import React from "react";
 import { cryptocurrencyList } from "../cryptocurrency-list";
 
-function Table() {
+// export const cryptocurrencyList = [
+//   {
+//     code: "BNB",
+//     name: "BNB",
+//     rate: 0.00311839,
+//   },
+//   {
+//     code: "BTC",
+//     name: "Bitcoin",
+//     rate: 0.00004066,
+//   },
+//   {
+//     code: "DOGE",
+//     name: "Dogecoin",
+//     rate: 11.18558722,
+//   },
+//   {
+//     code: "ETH",
+//     name: "Ethereum",
+//     rate: 0.00059237,
+//   },
+//   {
+//     code: "XRP",
+//     name: "XRP",
+//     rate: 2.50682634,
+//   },
+// ];
+
+
+function Table({exchangeAmount}) {
+
+    // display the data from cryptocurrencyList here in the table
+    // if exchangeAmount is empty or less than 0.01, show "n/a"
+
     return (
         <div className="card card-text mt-10 mx-4">
         <table className="mb-0">
@@ -13,11 +46,13 @@ function Table() {
             </tr>
             </thead>
             <tbody data-testid="exchange-data">
-            <tr>
-                <td>Currency Name</td>
-                <td>1 USD = Currency Rate Currency Code</td>
-                <td>n/a</td>
-            </tr>
+                {cryptocurrencyList.map((crypto) => (
+                    <tr key={crypto.code}>
+                        <td>{crypto.name}</td>
+                        <td>1 USD = {crypto.rate} {crypto.code}</td>
+                        <td>{exchangeAmount === "" ? (0).toFixed(8) : (Number(exchangeAmount) < 0.01 || Number(exchangeAmount) > 17042.67 ? "n/a" : (Number(exchangeAmount) * crypto.rate).toFixed(8))}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
         </div>
